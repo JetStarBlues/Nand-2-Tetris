37d085fd74b43022437a4deb75cdca23a7a93289
JetStarBlues <JetStarBlues@users.noreply.github.com>
2016-04-27 20:01:13 -0600

    more FSMs

---
+++ Other/FSMs/robotControl.py
@@ -0,0 +1,118 @@
+''''''''''''''''''''''''''' imports '''''''''''''''''''''''''''''
+
+# Built ins
+import sys
+
+# Computer files
+sys.path.append('../../Modules')
+from _1__elementaryGates import *
+from _2__arithmeticGates import *
+from _3__clock import *
+from _4__flipFlops import *
+
+
+''''''''''''''''''''''''' helpers '''''''''''''''''''''''''
+
+def toString(array):
+	return ''.join( map(str, array) )
+
+def toDecimal(bitSeq):
+	return int(bitSeq, 2)
+
+clock = Clock()
+delayRecording = clock.halfPeriod * 0.9
+
+'''
+When updating period values, consider:
+	1) clock's half period
+	2) FF propogation delay
+		> faux/simulated
+		> how long take for Q to update to new inputs
+	3) record delay 
+		> wait till Q/output values settled before recording/reading them.
+		> Selection range -> immediately after FF Propogation delay .. before end of second halfTick
+'''
+
+''''''''''''''''''''''''' main '''''''''''''''''''''''''
+
+nStages = 3
+dff = []
+for i in range(nStages): dff.append( DFlipFlop() )
+
+# start with all gates reset
+for i in range(nStages): dff[i].clear()
+
+dataIn = [0,1,1,1,0,1,1,0,1,0,0,1,1,0,0,0,0,0,1,1,1,1,0,1,0,1,1,0,0,0,0,0,1,0,0,1,0]
+dataIdx = len(dataIn) - 1  # load RtoL
+
+def logState( bumped, state ):
+	state = toString( state )
+	if state == '000': print( "forward at 25% speed >" )
+	if state == '001': print( "forward at 50% speed >>" )
+	if state == '010': print( "forward at 75% speed >>>" )
+	if state == '011': print( "forward at full speed >>>>" )
+	if state == '100': print( "reverse at 25% speed <" )
+	if state == '101': print( "reverse at 50% speed <<" )
+	if state == '110': print( "reverse at 75% speed <<<" )
+	if state == '111': print( "reverse at full speed <<<<" )
+	if bumped: print( "- hit something" )
+	# else: print( "- coast is clear" )
+
+def RC(clk):
+	'''changes direction when hits something'''
+
+	global dataIn
+	global dataIdx
+
+	data = dataIn[dataIdx]
+
+	dff[0].doTheThing( 
+		clk,
+		and_(
+			not_( data ),
+			or_( dff[0]._q1, dff[1].q1 )
+		)
+	)
+	dff[1].doTheThing( 
+		clk,
+		and_(
+			not_( data ),
+			or_( dff[0].q1, dff[1].q1 )
+		)		
+	)
+	dff[2].doTheThing( 
+		clk,
+		xor_( data, dff[2].q1 )
+	)
+
+	#
+	if dataIdx >= 0 : 
+
+		dataIdx -= 1
+
+		# Record output
+		global delayRecording
+		time.sleep(delayRecording)
+		logState( data, [ dff[2].q1, dff[1].q1, dff[0].q1 ] )
+
+	else:
+		print(".")
+
+
+''''''''''''''''''''''''' run '''''''''''''''''''''''''
+
+def main():
+	global clock 
+	clock.halfTick()
+
+	if clock.isRising:
+		RC(clock.value)
+
+	clock.keepTicking(1, main)  # seconds
+
+
+# Run
+if __name__ == '__main__':
+	main()
+
+# python latchTest.py > output.txt
---
+++ Other/FSMs/vendingMachine.py
@@ -0,0 +1,205 @@
+''''''''''''''''''''''''''' imports '''''''''''''''''''''''''''''
+
+# Built ins
+import sys
+
+# Computer files
+sys.path.append('../../Modules')
+from _1__elementaryGates import *
+from _2__arithmeticGates import *
+from _3__clock import *
+from _4__flipFlops import *
+
+
+''''''''''''''''''''''''' helpers '''''''''''''''''''''''''
+
+def toString(array):
+	return ''.join( map(str, array) )
+
+def toDecimal(bitSeq):
+	return int(bitSeq, 2)
+
+clock = Clock()
+delayRecording = clock.halfPeriod * 0.9
+
+'''
+When updating period values, consider:
+	1) clock's half period
+	2) FF propogation delay
+		> faux/simulated
+		> how long take for Q to update to new inputs
+	3) record delay 
+		> wait till Q/output values settled before recording/reading them.
+		> Selection range -> immediately after FF Propogation delay .. before end of second halfTick
+'''
+
+''''''''''''''''''''''''' main '''''''''''''''''''''''''
+
+nStages = 3
+dff = []
+for i in range(nStages): dff.append( DFlipFlop() )
+
+# start with all gates reset
+for i in range(nStages): dff[i].clear()
+
+user_input = [0,1,1,1,0,1,1,0,1,0,0,1,1,0,0,0,0,0,1,1,1,1,0,1,0,1,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,1,1]
+user_input_idx = 0
+x = [0] * 5
+
+def logState( thing, state ):
+	state = toString( state )
+
+	if thing == 'state':
+		if state == '000': print( 'rst' )
+		elif state == '001': print( 'D' )
+		elif state == '010': print( 'DN' )
+		elif state == '011': print( 'N' )
+		elif state == '100': print( 'ND' )
+		elif state == '101': print( 'NNN' )
+		elif state == '110': print( 'NN' )
+
+	elif thing == 'input':
+		if state == '00': print( 'you inserted a nickel' )
+		elif state == '01': print( 'you inserted a dime' )
+		elif state == '10': print( 'you inserted a quarter' )
+		else: print( 'you inserted an invalid coin, say adios to your previous money' )
+
+	elif thing == 'dispense':
+		if state == '0': print( 'need moar cash' )
+		elif state == '1': print( '*** tasty snack of your choice has been dispensed ***')
+
+	elif thing == 'change':
+		if state == '000': pass # print( 'no change bud' )
+		elif state == '001': print( 'your change is 5 cents' )
+		elif state == '010': print( 'your change is 10 cents' )
+		elif state == '011': print( 'your change is 15 cents' )
+		elif state == '100': print( 'your change is 20 cents' )
+
+
+def customPLA( x4, x3, x2, x1, x0 ):
+
+	# https://www.cs.umd.edu/class/sum2003/cmsc311/Notes/Comb/pla.html
+	'''
+	z6 = 01100 + 01100 + 11000 # s2
+	z5 = 00000 + 00100 + 01100 # s1
+	z4 = 00000 + 00001 + 11000 # s0
+
+	z3 = 00010 + 00101 + 00110 + 01000 + 01001 + 01010 + 01110 + 10000 + 10001 + 10010 + 10100 + 10101 + 10110 + 11001 + 11010 # dispense
+
+	z2 = 01010 + 10010 + 10110 # c2
+	z1 = 00110 + 01110 + 11010 # c1
+	z0 = 00010 + 00110 + 01001 + 10001 + 10101 + 11010 # c0  '''
+
+	# s2 s1 s0
+	z6 = orNto1_( [
+		andNto1_( [ not_( x4 ), x3, x2, not_( x1 ), not_( x0 ) ] ),
+		andNto1_( [ not_( x4 ), x3, x2, not_( x1 ), not_( x0 ) ] ),
+		andNto1_( [ x4, x3, not_( x2 ), not_( x1 ), not_( x0 ) ] ),
+	] )
+	z5 = orNto1_( [
+		andNto1_( [ not_( x4 ), not_( x3 ), not_( x2 ), not_( x1 ), not_( x0 ) ] ),
+		andNto1_( [ not_( x4 ), not_( x3 ), x2, not_( x1 ), not_( x0 ) ] ),
+		andNto1_( [ not_( x4 ), x3, x2, not_( x1 ), not_( x0 ) ] )
+	] )
+	z4 = orNto1_( [
+		andNto1_( [ not_( x4 ), not_( x3 ), not_( x2 ), not_( x1 ), not_( x0 ) ] ),
+		andNto1_( [ not_( x4 ), not_( x3 ), not_( x2 ), not_( x1 ), x0 ] ),
+		andNto1_( [ x4, x3, not_( x2 ), not_( x1 ), not_( x0 ) ] )
+	] )
+
+	# dispense
+	z3 = orNto1_( [
+		andNto1_( [ not_( x4 ), not_( x3 ), not_( x2 ), x1, not_( x0 ) ] ),
+		andNto1_( [ not_( x4 ), not_( x3 ), x2, not_( x1 ), x0 ] ),
+		andNto1_( [ not_( x4 ), not_( x3 ), x2, x1, not_( x0 ) ] ),
+		andNto1_( [ not_( x4 ), x3, not_( x2 ), not_( x1 ), not_( x0 ) ] ),
+		andNto1_( [ not_( x4 ), x3, not_( x2 ), not_( x1 ), x0 ] ),
+		andNto1_( [ not_( x4 ), x3, not_( x2 ), x1, not_( x0 ) ] ),
+		andNto1_( [ not_( x4 ), x3, x2, x1, not_( x0 ) ] ),
+		andNto1_( [ x4, not_( x3 ), not_( x2 ), not_( x1 ), not_( x0 ) ] ),
+		andNto1_( [ x4, not_( x3 ), not_( x2 ), not_( x1 ), x0 ] ),
+		andNto1_( [ x4, not_( x3 ), not_( x2 ), x1, not_( x0 ) ] ),
+		andNto1_( [ x4, not_( x3 ), x2, not_( x1 ), not_( x0 ) ] ),
+		andNto1_( [ x4, not_( x3 ), x2, not_( x1 ), x0 ] ),
+		andNto1_( [ x4, not_( x3 ), x2, x1, not_( x0 ) ] ),
+		andNto1_( [ x4, x3, not_( x2 ), not_( x1 ), x0 ] ),
+		andNto1_( [ x4, x3, not_( x2 ), x1, not_( x0 ) ] )
+	] )
+
+	#c2 c1 c0
+	z2 = orNto1_( [
+		andNto1_( [ not_( x4 ), x3, not_( x2 ), x1, not_( x0 ) ] ),
+		andNto1_( [ x4, not_( x3 ), not_( x2 ), x1, not_( x0 ) ] ),
+		andNto1_( [ x4, not_( x3 ), x2, x1, not_( x0 ) ] )
+	] )
+	z1 = orNto1_( [
+		andNto1_( [ not_( x4 ), not_( x3 ), x2, x1, not_( x0 ) ] ),
+		andNto1_( [ not_( x4 ), x3, x2, x1, not_( x0 ) ] ),
+		andNto1_( [ x4, x3, not_( x2 ), x1, not_( x0 ) ] )
+	] )
+	z0 = orNto1_( [
+		andNto1_( [ not_( x4 ), not_( x3 ), not_( x2 ), x1, not_( x0 ) ] ),
+		andNto1_( [ not_( x4 ), not_( x3 ), x2, x1, not_( x0 ) ] ),
+		andNto1_( [ not_( x4 ), x3, not_( x2 ), not_( x1 ), x0 ] ),
+		andNto1_( [ x4, not_( x3 ), not_( x2 ), not_( x1 ), x0 ] ),
+		andNto1_( [ x4, not_( x3 ), x2, not_( x1 ), x0 ] ),
+		andNto1_( [ x4, x3, not_( x2 ), x1, not_( x0 ) ] )
+	] )
+
+	return ( z6, z5, z4, z3, z2, z1, z0 )
+
+
+def VM(clk):
+	'''vending machine'''
+
+	global user_input
+	global user_input_idx
+	global x
+	
+	x[1] = user_input[user_input_idx]
+	x[0] = user_input[user_input_idx + 1]
+	x[4] = dff[0].q1
+	x[3] = dff[1].q1
+	x[2] = dff[2].q1
+
+	out = customPLA( x[4], x[3], x[2], x[1], x[0] )
+	
+	dff[0].doTheThing( clk, out[0] )
+	dff[1].doTheThing( clk, out[1] )
+	dff[2].doTheThing( clk, out[2] )
+
+	#
+	if user_input_idx < len( user_input ) - 2: 
+
+		user_input_idx += 2
+
+		# Record output
+		global delayRecording
+		time.sleep(delayRecording)
+		logState( 'input', [ x[1], x[0] ] )
+		# logState( 'state', [ out[0], out[1], out[2] ] )
+		logState( 'dispense', [ out[3] ] )
+		logState( 'change', [ out[4], out[5], out[6] ] )
+		print('---')
+
+	else:
+		print(".")
+
+
+''''''''''''''''''''''''' run '''''''''''''''''''''''''
+
+def main():
+	global clock 
+	clock.halfTick()
+
+	if clock.isRising:
+		VM(clock.value)
+
+	clock.keepTicking(1, main)  # seconds
+
+
+# Run
+if __name__ == '__main__':
+	main()
+
+# python latchTest.py > output.txt
