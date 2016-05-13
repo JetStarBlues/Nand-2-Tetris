36226f095bc97ceb289af7ed9ff7740ec82e1288
JetStarBlues <JetStarBlues@users.noreply.github.com>
2016-05-12 19:00:33 -0600

    working on assembler

---
+++ Staging/test1.asm
@@ -0,0 +1,34 @@
+// --- Computes RAM[1] = 1 + 2 + ... RAM[0]
+// --- Usage: load a value to RAM[0]
+
+// Setup
+@16
+M = 1		// RAM[16] = 1 // represents i
+@17
+M = 0		// RAM[17] = 0 // represents sum
+
+// Main loop - break condition
+@16
+D = M
+@0
+D = D - M   
+@20
+D ; JGT		// if i > RAM[0], exit main loop, enter return loop
+
+// Main loop - calculations
+@16
+D = M
+@17
+M = D + M 	// sum += i
+@16
+M = M + 1 	// i += 1
+@4
+0 ; JMP 	// next iteration of main loop
+
+// Return loop
+@17
+D = M
+@1
+M = D 		// RAM[1] = sum
+@21
+0 ; JMP 	// run return loop indefinitely
