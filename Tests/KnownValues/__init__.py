e76b0f23e6b7be37dde43fd1927a5a2d09cd35e3
JetStarBlues <JetStarBlues@users.noreply.github.com>
2017-04-12 21:08:29 -0600

    cleanup

--- Tests/KnownValues/__init__.py
+++ Tests/KnownValues/__init__.py
@@ -1,6 +1,6 @@
 #
-from .kv_1__elementaryGates import *
-from .kv_2__arithmeticGates import *
+from .kv_1__elementary import *
+from .kv_2__arithmetic import *
 from .kv_3__memory import *
 
 pathTo_kv_4 = './Programs/Tests/Chapter_6_assembly/bin/'
