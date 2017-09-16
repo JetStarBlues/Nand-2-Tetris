96955a9ca14871cf26b9f195ebdd307c41948019
JetStarBlues <JetStarBlues@users.noreply.github.com>
2017-09-15 23:05:51 -0600

    debug wip

--- runProgram.py
+++ runProgram.py
@@ -1,5 +1,8 @@
 '''------------------------------ Imports ------------------------------'''
 
+# Built ins
+import os
+
 # Hack computer
 from Components import *
 
@@ -40,12 +43,16 @@ def update():
 		clock.stop()
 		print( 'See you later!' )
 
-	debug()
-	
-	# if breakpoint():
 
-	# 	clock.stop()
-	# 	print( 'Breakpoint reached' )
+	# debug()
+	debug2File()
+	
+
+	if breakpoint():
+
+		clock.stop()
+		# io.quitPygame()  # call crashes for some reason
+		print( 'Breakpoint reached' )
 
 
 
@@ -54,6 +61,7 @@ def update():
 def breakpoint():
 
 	# return computer.data_memory.RAM.registers[ 8000 ] == 6
+	# return computer.data_memory.RAM.registers[ 4 ] == 9999
 
 	pass
 
@@ -63,37 +71,86 @@ def debug():
 	# print( clock.currentCycle )
 
 	print( '{} ------------'.format( computer.CPU.programCounter.register.register ) )
+	print( '' )
+
 	print( 'SP    {}'.format( computer.data_memory.RAM.registers[ 0 ] ) )
 	print( 'LCL   {}'.format( computer.data_memory.RAM.registers[ 1 ] ) )
 	print( 'ARG   {}'.format( computer.data_memory.RAM.registers[ 2 ] ) )
 	print( 'THIS  {}'.format( computer.data_memory.RAM.registers[ 3 ] ) )
 	print( 'THAT  {}'.format( computer.data_memory.RAM.registers[ 4 ] ) )
-	# print( 'GP0   {}'.format( computer.data_memory.RAM.registers[ 13 ] ) )
-	# print( 'GP1   {}'.format( computer.data_memory.RAM.registers[ 14 ] ) )
-	# print( 'GP2   {}'.format( computer.data_memory.RAM.registers[ 15 ] ) )
-	print()
+	print( 'GP0   {}'.format( computer.data_memory.RAM.registers[ 13 ] ) )
+	print( 'GP1   {}'.format( computer.data_memory.RAM.registers[ 14 ] ) )
+	print( 'GP2   {}'.format( computer.data_memory.RAM.registers[ 15 ] ) )
+	print( '' )
 
 	# static 16..255
 	print( 'Static' )
 	for i in range( 16, 256 ):
 		print( '{:<3}  {}'.format( i, computer.data_memory.RAM.registers[ i ] ) )
-	print()
+	print( '' )
 
 	# stack 256..2047
 	print( 'Stack' )
 	for i in range( 256, computer.data_memory.RAM.registers[ 0 ] + 1 ):
 		print( '{:<4}  {}'.format( i, computer.data_memory.RAM.registers[ i ] ) )
-	print()
+	print( '' )
 
 	# heap 2048..16383
 	print( 'Heap' )
 	for i in range( 2048, 16384 ):
 		print( '{:<5}  {}'.format( i, computer.data_memory.RAM.registers[ i ] ) )
-	print()
+	print( '' )
+
+
+debugPath = 'C:/Users/Janet/Desktop/Temp/DumpDebug/'
+
+def debug2File():
+
+	filePath = debugPath + str( clock.currentCycle )
+
+	with open( filePath, 'w' ) as file:
+
+		file.write( '{} ------------'.format( computer.CPU.programCounter.register.register ) + '\n' )
+		file.write( '' + '\n' )
+
+		file.write( 'SP    {}'.format( computer.data_memory.RAM.registers[ 0 ] ) + '\n' )
+		file.write( 'LCL   {}'.format( computer.data_memory.RAM.registers[ 1 ] ) + '\n' )
+		file.write( 'ARG   {}'.format( computer.data_memory.RAM.registers[ 2 ] ) + '\n' )
+		file.write( 'THIS  {}'.format( computer.data_memory.RAM.registers[ 3 ] ) + '\n' )
+		file.write( 'THAT  {}'.format( computer.data_memory.RAM.registers[ 4 ] ) + '\n' )
+		file.write( 'GP0   {}'.format( computer.data_memory.RAM.registers[ 13 ] ) + '\n' )
+		file.write( 'GP1   {}'.format( computer.data_memory.RAM.registers[ 14 ] ) + '\n' )
+		file.write( 'GP2   {}'.format( computer.data_memory.RAM.registers[ 15 ] ) + '\n' )
+		file.write( '' + '\n' )
+
+		# static 16..255
+		file.write( 'Static' + '\n' )
+		for i in range( 16, 256 ):
+			file.write( '{:<3}  {}'.format( i, computer.data_memory.RAM.registers[ i ] ) + '\n' )
+		file.write( '' + '\n' )
+
+		# stack 256..2047
+		file.write( 'Stack' + '\n' )
+		for i in range( 256, computer.data_memory.RAM.registers[ 0 ] + 1 ):
+			file.write( '{:<4}  {}'.format( i, computer.data_memory.RAM.registers[ i ] ) + '\n' )
+		file.write( '' + '\n' )
+
+		# heap 2048..16383
+		file.write( 'Heap' + '\n' )
+		for i in range( 2048, 16384 ):
+			file.write( '{:<5}  {}'.format( i, computer.data_memory.RAM.registers[ i ] ) + '\n' )
+		file.write( '' + '\n' )
+
 
 
 '''----------------------------- Run -----------------------------------'''
 
+# Remove existing logs
+for f in os.listdir( debugPath ): os.remove( debugPath + f )
+
+# Setup callbacks
 clock.callbackRising = update
+
+# Start
 clock.run()
 print( 'Program has started' )
