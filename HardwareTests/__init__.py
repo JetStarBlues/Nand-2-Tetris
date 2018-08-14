81b3749792c670761ce4db7496129f11e1a5ddda
JetStarBlues <JetStarBlues@users.noreply.github.com>
2018-08-13 19:41:46 -0600

    hardware tests update wip

--- HardwareTests/__init__.py
+++ HardwareTests/__init__.py
@@ -1,6 +1,12 @@
 # Hack Computer
 from Components import *
 
+#
+if PERFORMANCE_MODE:
+
+	raise Exception( 'HardwareTests only work when GC.PERFORMANCE_MODE is False' )
+
+
 # Tests
 from HardwareTests._x__testingHelpers import *
 from HardwareTests.KnownValues import *
