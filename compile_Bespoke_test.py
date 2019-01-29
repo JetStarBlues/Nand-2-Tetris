cb76ec10510f9e40f8f1fa6d7854698e04083617
JetStarBlues <JetStarBlues@users.noreply.github.com>
2019-01-29 15:10:40 -0700

    added CMP

--- compile_Bespoke_test.py
+++ compile_Bespoke_test.py
@@ -7,18 +7,15 @@ import Assembler.asm2bin_Bespoke
 
 # inputDirPath = '../N2T_Code/Programs/Assembly/Assembly/Tests/test1_add'
 # inputDirPath = '../N2T_Code/Programs/Assembly/Assembly/Tests/test2_flip'
-# inputDirPath = '../N2T_Code/Programs/Assembly/Assembly/Tests/test3_add'
 # inputDirPath = '../N2T_Code/Programs/Assembly/Assembly/Tests/test4_gt0'
-# inputDirPath = '../N2T_Code/Programs/Assembly/Assembly/Tests/test5_array'
+inputDirPath = '../N2T_Code/Programs/Assembly/Assembly/Tests/test5_array'
 # inputDirPath = '../N2T_Code/Programs/Assembly/Assembly/Tests/test5a_array'
 # inputDirPath = '../N2T_Code/Programs/Assembly/Assembly/Tests/test6_mult'
 # inputDirPath = '../N2T_Code/Programs/Assembly/Assembly/Tests/test7_fill'
 # inputDirPath = '../N2T_Code/Programs/Assembly/Assembly/Tests/test8_rect_buffer'
 # inputDirPath = '../N2T_Code/Programs/Assembly/Assembly/Tests/test8_rect_cmd'
-# inputDirPath = '../N2T_Code/Programs/Assembly/Assembly/Tests/test11_xor'
-# inputDirPath = '../N2T_Code/Programs/Assembly/Assembly/Tests/test12_shiftLeft'
-# inputDirPath = '../N2T_Code/Programs/Assembly/Assembly/Tests/test13_shiftRight'
-inputDirPath = '../N2T_Code/Programs/Assembly/Assembly/Tests/test14_linkRegister'
+# inputDirPath = '../N2T_Code/Programs/Assembly/Assembly/Tests/test11_miscArith'
+# inputDirPath = '../N2T_Code/Programs/Assembly/Assembly/Tests/test14_linkRegister'
 # inputDirPath = '../N2T_Code/Programs/Assembly/Assembly/Tests/test15_addressVariants'
 
 
